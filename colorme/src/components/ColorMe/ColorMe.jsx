90d81336c144438f93a12f0746db78e016aa156a
Isabel Klint <isabel.klint@gmail.com>
2024-06-11 20:01:12 -0600

    remove circle

--- colorme/src/components/ColorMe/ColorMe.jsx
+++ colorme/src/components/ColorMe/ColorMe.jsx
@@ -18,17 +18,16 @@ export const ColorMe = memo(function ColorMe({color, onChange}) {
   const padding = { padding: '0.5rem 0'}
 
   return (
-<div>
-        <div style={padding}>
-          <div className="w-72 h-72 rounded-full overflow-hidden relative">
-            <div className="absolute inset-0 flex items-center justify-center">
-              <div className="w-full h-full" style={{ clipPath: 'circle(50% at 50% 50%)' }}>
-                <Saturation width={288} height={288} color={color} onChange={onChange} />
-              </div>
+    <div >
+      <div style={padding}>
+        <div className="relative w-72 h-72 rounded-lg overflow-hidden bg-white">
+          <div className="absolute inset-0 flex items-center justify-center">
+            <div className="w-full h-full rounded-lg overflow-hidden">
+              <Saturation width={288} height={288} color={color} onChange={onChange} />
             </div>
           </div>
         </div>
-
+      </div>
       <div style={padding}>
         <Hue color={color} onChange={onChange} />
       </div>
@@ -39,8 +38,8 @@ export const ColorMe = memo(function ColorMe({color, onChange}) {
 
       <h1>Color: {formatRgba(color.rgb)}</h1>
       
-      <div style={divStyle}>
-      </div>
+      <div className="w-24 h-24 rounded-full" style={{ backgroundColor: rgba(...colorArr) }}></div>
+
 
       <Fields color={color} onChange={onChange} />
     </div>
