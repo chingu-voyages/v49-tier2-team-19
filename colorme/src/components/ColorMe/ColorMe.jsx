2a15ca9ca844c138007c0dea370eeaae18620841
Isabel Klint <isabel.klint@gmail.com>
2024-06-11 18:48:24 -0600

    circle

--- colorme/src/components/ColorMe/ColorMe.jsx
+++ colorme/src/components/ColorMe/ColorMe.jsx
@@ -18,10 +18,16 @@ export const ColorMe = memo(function ColorMe({color, onChange}) {
   const padding = { padding: '0.5rem 0'}
 
   return (
-    <div >
-      <div style={padding }>
-        <Saturation height={300} color={color} onChange={onChange} />
-      </div>   
+<div>
+        <div style={padding}>
+          <div className="w-72 h-72 rounded-full overflow-hidden relative">
+            <div className="absolute inset-0 flex items-center justify-center">
+              <div className="w-full h-full" style={{ clipPath: 'circle(50% at 50% 50%)' }}>
+                <Saturation width={288} height={288} color={color} onChange={onChange} />
+              </div>
+            </div>
+          </div>
+        </div>
 
       <div style={padding}>
         <Hue color={color} onChange={onChange} />
